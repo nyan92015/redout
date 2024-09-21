4efad477d70e2842b79103f5ba7d27a6e2382106
Hiromi <75907477+nyan92015@users.noreply.github.com>
2024-09-22 08:43:44 +0900

    add cors

--- src/server.js
+++ src/server.js
@@ -1,13 +1,28 @@
 import { Server } from 'boardgame.io/server';
 import serve from 'koa-static';
+import Koa from 'koa';
+import cors from '@koa/cors'; // CORSミドルウェアのインポート
 import { TicTacToe } from './Game';
 
-const server = Server({ name: 'tic-tac-toe', games: [TicTacToe],origins: [
-    'https://redout.onrender.com',
-    'localhost:8000'
-  ],});
+const app = new Koa();
+const server = Server({ 
+  name: 'tic-tac-toe', 
+  games: [TicTacToe],
+});
+
 const PORT = process.env.PORT || 8000;
 
-server.app.use(serve('./build'))
+// CORS設定
+app.use(cors({
+  origin: [
+    'https://redout.onrender.com',
+    'http://localhost:8000' // スキームを含めて指定する
+  ],
+}));
 
-server.run(PORT)
+// 静的ファイルの提供
+app.use(serve('./build'));
+
+// サーバーを実行
+server.app = app; // ServerのappをKoaのappに設定
+server.run(PORT);
