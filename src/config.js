ad61fe3e9279060b1add99e20178bc1a5dafbfc6
Hiromi <75907477+nyan92015@users.noreply.github.com>
2024-09-26 21:18:30 +0900

    fix url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import { LobbyClient } from 'boardgame.io/client';
 
-export const SERVER_URL = 'http://localhost:8000';
+export const SERVER_URL = 'https://redout.onrender.com';
 export const gameName = 'tic-tac-toe';
 export const lobbyClient = new LobbyClient({ server: SERVER_URL });
 
