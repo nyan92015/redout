5f400608e1106481d1da8b6e0bdc30c271cef758
Hiromi <75907477+nyan92015@users.noreply.github.com>
2024-10-01 22:21:31 +0900

    fix

--- src/components/CardBoard/index.jsx
+++ src/components/CardBoard/index.jsx
@@ -7,7 +7,7 @@ const CardBoard = ({ cardBoardName, children }) => {
   const constraintsRef = useRef(null);
   return (
     <motion.div ref={constraintsRef}>
-      <Stagger className={`card-board ${cardBoardName}`} staggerDelay={0.4}>
+      <Stagger className={`card-board ${cardBoardName}`} staggerDelay={0.2}>
       {children}
       </Stagger>
     </motion.div>
